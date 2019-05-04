ddd8bbdee8266d34c017b4d6a4fb2ce140e576eb
Dan <headhertz88@gmail.com>
2019-05-04 17:21:05 +0100

    basic head/meta info

---
+++ client/containers/Meta.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+const Meta = () => (
+	<Head>
+		<meta
+			key="viewport"
+			content="width=device-width, initial-scale=1,  maximum-scale=1, user-scalable=0"
+			name="viewport"
+		/>
+		<meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+		{/* <link rel="shortcut icon" href="/static/favicon.png" /> */}
+		{/* <link rel="stylesheet" type="text/css" href="" /> */}
+		<title>The Greatest Park Never Built</title>
+		<meta name="description" content="TODO" />
+	</Head>
+);
+
+export default Meta;
