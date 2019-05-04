7bc85f7a6c55e14feb72c39514bdb7ba978de29e
Dan <headhertz88@gmail.com>
2019-05-04 20:26:22 +0100

    adds elementary global context

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,53 @@
 import App, { Container } from 'next/app';
 import React from 'react';
-import NProgress from 'next-nprogress/component';
-// import Page from '../containers/Page';
+import Router from 'next/router';
+// Import NProgress from 'next-nprogress/component';
+import UserContext from '../components/UserContext';
 
 class MyApp extends App {
+	state = {
+		user: true,
+		theme: 'dark'
+	};
+
+	static async getInitialProps( { Component, ctx } ) {
+		let pageProps = {};
+
+		if ( Component.getInitialProps ) {
+			pageProps = await Component.getInitialProps( ctx );
+		}
+
+		return { pageProps };
+	}
+
+	componentDidMount = () => {
+		const user = localStorage.getItem( 'gpnb-user' );
+
+		if ( this.state.user ) {
+			this.setState( {
+				user
+			} );
+		} else {
+			Router.push( '/signin' );
+		}
+	};
+
 	render() {
 		const { Component, pageProps } = this.props;
 
-		<Container>
-			<NProgress color="#F43779" options={{ trickleSpeed: 50 }} showAfterMs={0} />
-			<React.Fragment>
-				<Component {...pageProps} />
-			</React.Fragment>
-		</Container>;
+		return (
+			<Container>
+				{/* <NProgress color="#F43779" options={{ trickleSpeed: 50 }} showAfterMs={0} /> */}
+				<UserContext.Provider
+					value={{
+						user: this.state.user,
+						theme: this.state.theme
+					}}
+				>
+					<Component {...pageProps} />
+				</UserContext.Provider>
+			</Container>
+		);
 	}
 }
 
