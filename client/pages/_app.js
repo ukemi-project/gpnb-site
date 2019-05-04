8aca3d7c3465e84585191547ca8b778c7d9db520
Dan <headhertz88@gmail.com>
2019-05-04 17:27:58 +0100

    styled progress bar

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,13 +1,14 @@
 import App, { Container } from 'next/app';
 import React from 'react';
 import NProgress from 'next-nprogress/component';
-import component from 'next-nprogress/dist/component';
 // import Page from '../containers/Page';
 
 class MyApp extends App {
 	render() {
+		const { Component, pageProps } = this.props;
+
 		<Container>
-			<NProgress />
+			<NProgress color="#F43779" options={{ trickleSpeed: 50 }} showAfterMs={0} />
 			<React.Fragment>
 				<Component {...pageProps} />
 			</React.Fragment>
