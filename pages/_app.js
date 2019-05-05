2562cb2ee789bea4669355fdd37b4f5efc11ba1d
Dan <headhertz88@gmail.com>
2019-05-05 23:02:19 +0100

    load font awseome and bootstrap core

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,14 @@ import React from 'react';
 import Router from 'next/router';
 import UserContext from '../components/UserContext';
 
+import '@fortawesome/fontawesome-free/css/brands.css';
+import '@fortawesome/fontawesome-free/css/regular.css';
+import '@fortawesome/fontawesome-free/css/solid.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import 'simple-line-icons/css/simple-line-icons.css';
+
+import '../styles/app.scss';
+
 class MyApp extends App {
 	state = {
 		user: true,
