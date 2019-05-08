6d81ee2abbd684038d4599a41a0b39619c4dbd3c
Dan <headhertz88@gmail.com>
2019-05-08 11:34:44 +0100

    cleaned up server code, login form styles

--- pages/login.js
+++ pages/login.js
@@ -3,19 +3,16 @@ import React from 'react';
 const Login = () => (
 	<div className='block-center mt-4 wd-xl'>
 		<div className='card card-flat'>
-			<div className='card-header text-center bg-dark'>
-				<a href=''>
-					<img className='block-center rounded' src='/static/img/logo.png' alt='Logo' />
-				</a>
+			<div className='card-header text-center bg-info'>
+				{/* <img className='block-center rounded' src='/static/img/logo.png' alt='Logo' /> */}
+				<h5 className='block-center text-white'>GPNB</h5>
 			</div>
 			<div className='p-3 text-center'>
 				<span className='mr-2'>&copy;</span>
-				<span>2019</span>
-				<br />
-				<span>The Greatest Park Never Built</span>
+				<span>2019</span> - <span>The Greatest Park Never Built</span>
 				<br />
 				<span>
-					A project by <a href='https://ukemi.ninja'>UKEMI</a>
+					Maintained by @<a href='https://ukemi.ninja'>UkemiProject</a>
 				</span>
 			</div>
 		</div>
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import fileUpload from 'express-fileupload';
 import next from 'next';
+
 import path from 'path';
 import cluster from 'cluster';
 import bodyParser from 'body-parser';
 
 const numCPUs = require( 'os' ).cpus().length,
-	dev = process.env.NODE_ENV !== 'production';
+	dev = process.env.NODE_ENV !== 'production',
+	port = process.env.PORT || 3000,
+	routes = [ { src: '/login', dest: '/pages/login' } ];
 
 if ( !dev && cluster.isMaster ) {
 	console.log( `Node cluster master ${process.id} is running.` );
@@ -23,65 +26,63 @@ if ( !dev && cluster.isMaster ) {
 	const app = next( { dir: '.', dev } ),
 		handle = app.getRequestHandler();
 
-	app.prepare().then( () => {
-		const server = express(),
-			PORT = process.env.PORT || 5000;
+	app
+		.prepare()
+		.then( () => {
+			const server = express();
 
-		if ( !dev ) {
-			// Enforce SSL & HSTS in production
-			server.use( ( req, res, next ) => {
-				let proto = req.headers[ 'x-forwarded-proto' ];
+			if ( !dev ) {
+				// Enforce SSL & HSTS in production
+				server.use( ( req, res ) => {
+					let proto = req.headers[ 'x-forwarded-proto' ];
 
-				if ( proto === 'https' ) {
-					res.set( {
-						'Strict-Transport-Security': 'max-age=31557600' // One-year
-					} );
-					return next();
-				}
+					if ( proto === 'https' ) {
+						res.set( {
+							'Strict-Transport-Security': 'max-age=31557600' // One-year
+						} );
+						return next();
+					}
 
-				res.redirect( `https://${req.headers.host}${req.url}` );
-			} );
-		}
-
-		// Static files
-		// https://github.com/zeit/next.js/tree/4.2.3#user-content-static-file-serving-eg-images
-		server.use(
-			'/images',
-			express.static( path.join( __dirname, 'images' ), {
-				maxAge: dev ? '0' : '365d'
-			} )
-		);
-
-		server.use( fileUpload() ).use( bodyParser.json() );
-
-		server.get( '*', ( req, res ) => {
-			return handle( req, res );
-		} );
-
-		server.post( '/upload', ( req, res ) => {
-			if ( req.files === null ) {
-				return res.status( 400 ).json( { msg: 'No file uploaded' } );
+					res.redirect( `https://${req.headers.host}${req.url}` );
+				} );
 			}
 
-			const file = req.files.file;
+			// Static files
+			// https://github.com/zeit/next.js/tree/4.2.3#user-content-static-file-serving-eg-images
+			server.use(
+				'/images',
+				express.static( path.join( __dirname, 'images' ), {
+					maxAge: dev ? '0' : '365d'
+				} )
+			);
 
-			file.mv( `${__dirname}/public/uploads/${file.name}`, ( err ) => {
-				if ( err ) {
-					console.error( err );
+			server.use( fileUpload() ).use( bodyParser.json() );
 
-					return res.status( 500 ).send( err );
+			server.post( '/upload', ( req, res ) => {
+				if ( req.files === null ) {
+					return res.status( 400 ).json( { msg: 'No file uploaded.' } );
 				}
 
-				res.json( { fileName: file.name, filePath: `/uploads/${file.name}` } );
+				const file = req.files.file;
+
+				file.mv( `${__dirname}/public/uploads/${file.name}`, ( err ) => {
+					if ( err ) {
+						console.error( err );
+
+						return res.status( 500 ).send( err );
+					}
+
+					res.json( { fileName: file.name, filePath: `/uploads/${file.name}` } );
+				} );
 			} );
-		} );
 
-		server.listen( PORT, ( err ) => {
-			if ( err ) {
-				throw err;
-			}
+			routes.forEach( ( r ) => server.get( r.src, ( req, res ) => app.render( req, res, r.dest, req.params ) ) );
 
-			console.log( 'Server started...' );
-		} );
-	} );
+			server.get( '*', ( req, res ) => handle( req, res ) );
+
+			server.listen( port );
+
+			console.log( `> Ready on http://localhost:${port}` );
+		} )
+		.catch( ( err ) => console.log( err ) );
 }
