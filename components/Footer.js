3655d23bb18732da4c6573abb704ac887e3cc512
Dan <headhertz88@gmail.com>
2019-06-05 11:14:34 +0100

    Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const Footer = () => {
+	const year = new Date().getFullYear();
+
 	return (
-		<div>
-			{/*
-       */}
-			{/*  */}
-		</div>
+		<footer className='footer-container text-center'>
+			<span>
+				&copy; {year} - GPNB - A <a href='http://ukemi.ninja'>Ukemi</a> Project
+			</span>
+		</footer>
 	);
 };
 
--- containers/Layout.js
+++ containers/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from './Head';
+import Footer from '../components/Footer';
 
 const Layout = ( props ) => {
 	return (
@@ -7,9 +8,9 @@ const Layout = ( props ) => {
 			<Head />
 			{/* <Header /> */}
 			<section className='section-container'>{props.children}</section>
+			<Footer />
 		</div>
 	);
 };
-
 
 export default Layout;
